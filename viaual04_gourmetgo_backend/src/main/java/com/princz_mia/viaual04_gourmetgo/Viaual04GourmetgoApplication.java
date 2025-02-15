2cb2ae83318ec0a617e1dd9632ce155280e157e6
Princz Mia <princz.mia@gmail.com>
2025-02-15 22:17:45 +0100

    feat: initialize Spring Boot project

---
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo/Viaual04GourmetgoApplication.java
@@ -0,0 +1,13 @@
+package com.princz_mia.viaual04_gourmetgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Viaual04GourmetgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Viaual04GourmetgoApplication.class, args);
+	}
+
+}
---
+++ viaual04_gourmetgo_backend/src/test/java/com/princz_mia/viaual04_gourmetgo/Viaual04GourmetgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princz_mia.viaual04_gourmetgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Viaual04GourmetgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
