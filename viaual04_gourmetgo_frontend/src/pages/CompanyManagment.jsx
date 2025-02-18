72cd1e2b0a9346c82e418f313e3b06897799bb26
Princz Mia <princz.mia@gmail.com>
2025-02-18 21:22:53 +0100

    feat: add skeleton structure for pages

---
+++ viaual04_gourmetgo_frontend/src/pages/CompanyManagment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompanyManagment = () => {
+  return (
+    <div>CompanyManagment</div>
+  )
+}
+
+export default CompanyManagment
---
+++ viaual04_gourmetgo_frontend/src/pages/RestaurantManagment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RestaurantManagment = () => {
+  return (
+    <div>RestaurantManagment</div>
+  )
+}
+
+export default RestaurantManagment
