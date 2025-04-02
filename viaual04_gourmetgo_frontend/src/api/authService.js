198b3f6f2103085996e4bd379302c4fda675e060
Princz Mia <princz.mia@gmail.com>
2025-04-03 00:16:08 +0200

    feat: add authentication utilities

---
+++ viaual04_gourmetgo_frontend/src/api/authService.js
@@ -0,0 +1,62 @@
+import axiosInstance from "./axiosConfig";
+import { toast } from "react-toastify";
+import axios from "axios";
+
+export const getUser = () =>
+  localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null;
+
+export const login = async (loginRequest) => {
+  try {
+    const response = await axiosInstance.post("/auth/login", {
+      emailAddress: loginRequest.email,
+      password: loginRequest.password,
+    });
+    localStorage.setItem("user", JSON.stringify(response.data.data));
+    return response;
+  } catch (error) {
+    toast.error(
+      error.response?.data?.message || "An error occurred during login!"
+    );
+    return null;
+  }
+};
+
+export const logout = () => {
+  localStorage.removeItem("user");
+};
+
+export const registerCustomer = async (registerRequest) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:8080/api/v1/customers/register",
+      {
+        fullName: registerRequest.fullName,
+        emailAddress: registerRequest.email,
+        password: registerRequest.password,
+      }
+    );
+    console.log(response.data);
+    return response;
+  } catch (error) {
+    toast.error(
+      error.response?.data?.message || "An error occurred during registration!"
+    );
+  }
+  return null;
+};
+
+export const verify = async (key) => {
+  try {
+    const response = await axiosInstance.get(
+      `/customers/verify/account?key=` + key
+    );
+    return response;
+  } catch (error) {
+    toast.error(
+      error.response?.data?.message || "An error occurred during verification!"
+    );
+  }
+  return null;
+};
