205f5ad29b32b7f20b96bc9131ae2d93e4c407c9
Princz Mia <princz.mia@gmail.com>
2025-04-04 00:44:16 +0200

    feat: add account verification page

--- viaual04_gourmetgo_frontend/src/pages/Verification.jsx
+++ viaual04_gourmetgo_frontend/src/pages/Verification.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { verify } from '../api/authService';
 
 const Verification = () => {
-  return (
-    <div>Verification</div>
-  )
-}
+  const [searchParams] = useSearchParams();
+  const key = searchParams.get('key');
 
-export default Verification
+  useEffect(() => {
+    if (key) {
+      verify(key).then((response) => {
+        if (response) {
+          toast.success('Account verified successfully!');
+        }
+      });
+    }
+  }, [key, verify]);
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p>Verifying your account, please wait...</p>
+    </div>
+  );
+};
+
+export default Verification;
