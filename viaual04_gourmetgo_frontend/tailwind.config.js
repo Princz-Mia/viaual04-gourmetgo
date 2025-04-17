7fee2b64e1ecffd1963773dc43bdd1748e66330f
Princz Mia <princz.mia@gmail.com>
2025-04-18 00:33:38 +0200

    feat: add tailwind.config.js with DaisyUI plugin

---
+++ viaual04_gourmetgo_frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#570df8",
+          secondary: "#f000b8",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
+  },
+};
