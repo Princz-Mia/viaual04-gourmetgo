90062fff4fd54992d7d09c3dea2b17ad52b11c08
Princz Mia <princz.mia@gmail.com>
2025-04-18 00:48:40 +0200

    feat: delete StrictMode and add ToastContainer to be able to use toastify mesages

--- viaual04_gourmetgo_frontend/src/main.jsx
+++ viaual04_gourmetgo_frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { ToastContainer, Slide } from 'react-toastify';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
-)
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Slide}
+    />
+  </>
+);
