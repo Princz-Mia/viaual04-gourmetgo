c6dfae08a4e46b2489c36e89fe06e4a515e4095c
Princz Mia <princz.mia@gmail.com>
2025-04-26 19:57:07 +0200

    feat: add ConfirmationRepository for managing confirmation keys
    - Create ConfirmationRepository extending JpaRepository
    - Add method `findByKey` to lookup Confirmation entity by its key

---
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/confirmation/ConfirmationRepository.java
@@ -0,0 +1,12 @@
+package com.princz_mia.viaual04_gourmetgo_backend.confirmation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ConfirmationRepository extends JpaRepository<Confirmation, UUID> {
+
+    Confirmation findByKey(String key);
+}
