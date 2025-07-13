4b8672ee03e621cd6a98f4d3c2aa4ae363e67507
Princz Mia <princz.mia@gmail.com>
2025-07-13 15:09:45 +0200

    feat: add AddressRepository for Address entity persistence

---
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/address/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.princz_mia.viaual04_gourmetgo_backend.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
