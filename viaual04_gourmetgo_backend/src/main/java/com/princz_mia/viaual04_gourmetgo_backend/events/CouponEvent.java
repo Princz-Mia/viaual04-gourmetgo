5866b31afe5c2191c75432c0427f884596937d1d
Princz Mia <princz.mia@gmail.com>
2025-07-13 16:47:02 +0200

    feat: add CouponEvent class encapsulating coupon and event type

---
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/events/CouponEvent.java
@@ -0,0 +1,15 @@
+package com.princz_mia.viaual04_gourmetgo_backend.events;
+
+import com.princz_mia.viaual04_gourmetgo_backend.coupon.Coupon;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CouponEvent {
+
+    private Coupon coupon;
+    private EventType type;
+}
