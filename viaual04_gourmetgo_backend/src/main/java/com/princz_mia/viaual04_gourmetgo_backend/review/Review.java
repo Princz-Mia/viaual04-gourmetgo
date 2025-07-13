e03a46b17736787bcacc2075f85d8e32d80e01b6
Princz Mia <princz.mia@gmail.com>
2025-07-13 15:54:16 +0200

    feat: add Review entity with rating, comment, timestamps, and relations to customer and restaurant

--- viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/review/Review.java
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/review/Review.java
@@ -1,14 +1,21 @@
 package com.princz_mia.viaual04_gourmetgo_backend.review;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.princz_mia.viaual04_gourmetgo_backend.customer.Customer;
+import com.princz_mia.viaual04_gourmetgo_backend.product.Product;
+import com.princz_mia.viaual04_gourmetgo_backend.restaurant.Restaurant;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -20,6 +27,26 @@ import java.util.UUID;
 public class Review {
 
     @Id
-    @UuidGenerator(style = UuidGenerator.Style.AUTO)
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
     private UUID id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_id", referencedColumnName = "id")
+    private Restaurant restaurant;
+
+    @Min(1)
+    @Max(5)
+    private Integer ratingValue;
+
+    private String comment;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
