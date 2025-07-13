79fec26bd09fec4344e716b97a8ad357a8d3d957
Princz Mia <princz.mia@gmail.com>
2025-07-13 16:47:39 +0200

    feat: add EventType enum with application event constants

---
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/events/EventType.java
@@ -0,0 +1,9 @@
+package com.princz_mia.viaual04_gourmetgo_backend.events;
+
+public enum EventType {
+    REGISTRATION,
+    APPROVED,
+    RESET_PASSWORD,
+    ORDER_PLACED,
+    COUPON_PUBLISHED,
+}
