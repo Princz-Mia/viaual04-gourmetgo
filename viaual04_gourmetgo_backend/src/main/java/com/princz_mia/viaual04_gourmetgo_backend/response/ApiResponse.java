6d1047012060e21a89af059fba7811f7a37a63ae
Princz Mia <princz.mia@gmail.com>
2025-07-13 16:50:37 +0200

    feat: add ApiResponse DTO for wrapping API response messages and data

---
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/response/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.princz_mia.viaual04_gourmetgo_backend.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private Object data;
+}
