1e4a3422cd84f86b3806e03aceeaab29cd38a826
Princz Mia <princz.mia@gmail.com>
2025-07-13 16:41:51 +0200

    refactor: add Component annotation and final keyword to attributes

--- viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/config/security/jwt/JWTAuthenticationFilter.java
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/config/security/jwt/JWTAuthenticationFilter.java
@@ -8,17 +8,21 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
-    private JWTTokenProvider tokenProvider;
-    private CustomUserDetailsService customerDetailsService;
+    private final JWTTokenProvider tokenProvider;
+    private final CustomUserDetailsService customerDetailsService;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
