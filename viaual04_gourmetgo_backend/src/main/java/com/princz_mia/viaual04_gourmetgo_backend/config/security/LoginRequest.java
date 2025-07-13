93c9d4b66133a93e3886bee1e9d2ea41c0551933
Princz Mia <princz.mia@gmail.com>
2025-07-13 16:43:53 +0200

    refactor: add annotations

--- viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/config/security/LoginRequest.java
+++ viaual04_gourmetgo_backend/src/main/java/com/princz_mia/viaual04_gourmetgo_backend/config/security/LoginRequest.java
@@ -2,12 +2,18 @@ package com.princz_mia.viaual04_gourmetgo_backend.config.security;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
 
-    @Email  
+    @Email
     private String emailAddress;
 
     @NotBlank
