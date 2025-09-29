12e80f450e0953722d5e03294712f8a1677af7b5
Princz Mia <princz.mia@gmail.com>
2025-09-29 20:18:03 +0200

    feat: add custom status enums

---
+++ viaual04_gourmetgo_frontend/src/utils/statusUtils.js
@@ -0,0 +1,29 @@
+export const OrderStatuses = [
+  "PENDING",
+  "CONFIRMED",
+  "PREPARING",
+  "READY_FOR_PICKUP",
+  "OUT_FOR_DELIVERY",
+  "DELIVERED",
+  "CANCELLED",
+];
+
+export const StatusLabels = {
+  PENDING: "Pending",
+  CONFIRMED: "Confirmed",
+  PREPARING: "Preparing",
+  READY_FOR_PICKUP: "Ready for Pickup",
+  OUT_FOR_DELIVERY: "Out for Delivery",
+  DELIVERED: "Delivered",
+  CANCELLED: "Cancelled",
+};
+
+export const StatusClasses = {
+  PENDING: "badge badge-info text-right w-32",
+  CONFIRMED: "badge badge-primary text-right w-32",
+  PREPARING: "badge badge-warning text-right w-32",
+  READY_FOR_PICKUP: "badge badge-secondary text-right w-32",
+  OUT_FOR_DELIVERY: "badge badge-info text-right w-32",
+  DELIVERED: "badge badge-success text-right w-32",
+  CANCELLED: "badge badge-error text-right w-32",
+};
