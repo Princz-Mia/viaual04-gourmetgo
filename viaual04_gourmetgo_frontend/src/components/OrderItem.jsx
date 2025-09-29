f106838c479b95529567d1cdec959199fdfc0fb4
Princz Mia <princz.mia@gmail.com>
2025-09-29 20:13:15 +0200

    feat: add image fetching logic

--- viaual04_gourmetgo_frontend/src/components/OrderItem.jsx
+++ viaual04_gourmetgo_frontend/src/components/OrderItem.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import imageNotFound from "../assets/images/image_not_found.jpg"
 
 export default function OrderItem({ item }) {
-    // item: { id, product: { name, image, price }, quantity }
-    const { product, quantity } = item;
+    const { product, quantity, price, } = item;
     const lineTotal = product.price * quantity;
 
     return (
         <div className="bg-white shadow rounded-lg p-4 flex items-center gap-4">
             <img
-                src={product.image}
+                src={
+                    product.image
+                        ? `http://localhost:8080${product.image}`
+                        : imageNotFound
+                }
                 alt={product.name}
                 className="w-20 h-20 object-cover rounded"
+                onError={(e) => {
+                    e.currentTarget.onerror = null; // végtelen ciklus elkerülése
+                    e.currentTarget.src = imageNotFound;
+                }}
             />
             <div className="flex-1">
                 <h4 className="font-semibold">{product.name}</h4>
