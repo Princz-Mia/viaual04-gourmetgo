16061772bd4e0742281a1a77032242bf09d55811
Princz Mia <princz.mia@gmail.com>
2025-09-29 20:00:47 +0200

    feat: import getUser function from authService and add credentials

--- viaual04_gourmetgo_frontend/src/api/axiosConfig.js
+++ viaual04_gourmetgo_frontend/src/api/axiosConfig.js
@@ -1,12 +1,15 @@
 import axios from "axios";
+import { getUser } from "./authService";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8080/api/v1",
 });
 
+axiosInstance.defaults.withCredentials = true;
+
 axiosInstance.interceptors.request.use(
   (config) => {
-    const user = localStorage.getItem("user");
+    const user = getUser();
     const token = user?.token;
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
