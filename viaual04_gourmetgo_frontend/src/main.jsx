a101586026c5ebd3f214610efd636d87604583a0
Princz Mia <princz.mia@gmail.com>
2025-09-30 20:13:09 +0200

    build: insignificant changes and package managment

--- viaual04_gourmetgo_frontend/src/main.jsx
+++ viaual04_gourmetgo_frontend/src/main.jsx
@@ -4,22 +4,25 @@ import { ToastContainer, Slide } from 'react-toastify';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from './contexts/AuthContext.jsx';
+import { CartProvider } from './contexts/CartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <App />
-    <ToastContainer
-      position="top-right"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-      transition={Slide}
-    />
+    <CartProvider>
+      <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
+    </CartProvider>
   </AuthProvider>,
 );
